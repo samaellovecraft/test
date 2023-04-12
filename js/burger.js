6269b5c40fdf1f2696c78f3adfa3a847bbd12275
Samael Lovecraft <samael69lovecraft@gmail.com>
2023-04-12 08:07:36 +0300

    title

--- js/burger.js
+++ js/burger.js
@@ -9,3 +9,17 @@ $(document).ready(function() {
     $('.museum').toggleClass('active');
   });
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.querySelector('.navbar__burger').addEventListener('click', function(event) {
+//     document.querySelectorAll('.navbar__burger,.navbar__menu,.navbar__list li').forEach(function(element) {
+//       element.classList.toggle('active');
+//     });
+//     document.querySelector('.artworks-area').classList.toggle('lock');
+//     document.querySelector('.navbar').classList.toggle('active');
+//   });
+//   document.querySelector('.museum').addEventListener('click', function(event) {
+//     document.querySelector('.museum-item').classList.toggle('active');
+//     document.querySelector('.museum').classList.toggle('active');
+//   });
+// });
